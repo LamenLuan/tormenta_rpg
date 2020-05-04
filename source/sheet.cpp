ec6815bbe1e93cde76ab9b18c1cde2ac9ea8fbb3
Luan Leme <luan.leme.s35@gmail.com>
2020-05-04 10:15:52 -0300

    Trying to use lambda functions to avoid repetition

--- source/sheet.cpp
+++ source/sheet.cpp
@@ -1,42 +1,39 @@
 #include "../headers/sheet.hpp"
 
 #include <memory>
+#include <functional>
 
-Sheet::Sheet()
-{
-}
+Sheet::Sheet() = default;
 
 Sheet::Sheet(std::string t_name, uint8_t t_strength, uint8_t t_dexterity,
     uint8_t t_constitution, uint8_t t_inteligence,
     uint8_t t_wisdom, uint8_t t_charisma, uint8_t t_level,
     unsigned short t_maxLife)
     :
-    m_name(t_name), m_strength(t_strength), m_dexterity(t_dexterity),
+    m_name(std::move(t_name)), m_strength(t_strength), m_dexterity(t_dexterity),
     m_constitution(t_constitution), m_inteligence(t_inteligence),
     m_wisdom(t_wisdom), m_charisma(t_charisma), m_level(t_level),
     m_maxLife(t_maxLife), m_currentLife(t_maxLife)
 {
 }
 
-Sheet::~Sheet()
-{
-}
+Sheet::~Sheet() = default;
 
 const std::string &Sheet::get_name() const { return m_name; }
 
-unsigned short Sheet::get_level() const { return m_level; }
+uint8_t Sheet::get_level() const { return m_level; }
 
-unsigned short Sheet::get_strength() const { return m_strength; }
+uint8_t Sheet::get_strength() const { return m_strength; }
 
-unsigned short Sheet::get_dexterity() const { return m_dexterity; }
+uint8_t Sheet::get_dexterity() const { return m_dexterity; }
 
-unsigned short Sheet::get_constitution() const { return m_constitution; }
+uint8_t Sheet::get_constitution() const { return m_constitution; }
 
-unsigned short Sheet::get_inteligence() const { return m_inteligence; }
+uint8_t Sheet::get_inteligence() const { return m_inteligence; }
 
-unsigned short Sheet::get_wisdom() const { return m_wisdom; }
+uint8_t Sheet::get_wisdom() const { return m_wisdom; }
 
-unsigned short Sheet::get_charisma() const { return m_charisma; }
+uint8_t Sheet::get_charisma() const { return m_charisma; }
 
 unsigned short Sheet::get_maxLife() const { return m_maxLife; }
 
@@ -49,44 +46,44 @@ void Sheet::set_name(const std::string& t_name)
     if ( !t_name.empty() ) m_name = t_name;
 }
 
-void Sheet::set_level(short t_level)
+void Sheet::set_level(uint8_t t_level)
 {
     if(t_level > 0 && t_level <= 20) m_level = t_level;
 }
 
-void Sheet::set_strength(short t_strength)
+void Sheet::set_strength(uint8_t t_strength)
 {
     if(t_strength > 0) m_strength = t_strength;
 }
 
-void Sheet::set_dexterity(short t_dexterity)
+void Sheet::set_dexterity(uint8_t t_dexterity)
 {
     if(t_dexterity > 0) m_dexterity = t_dexterity;
 }
 
-void Sheet::set_constitution(short t_constitution)
+void Sheet::set_constitution(uint8_t t_constitution)
 {
     if(t_constitution > 0) m_constitution = t_constitution;
 }
 
-void Sheet::set_inteligence(short t_inteligence)
+void Sheet::set_inteligence(uint8_t t_inteligence)
 {
     if(t_inteligence > 0) m_inteligence = t_inteligence;
 }
 
-void Sheet::set_wisdom(short t_wisdom)
+void Sheet::set_wisdom(uint8_t t_wisdom)
 {
     if(t_wisdom > 0) m_wisdom = t_wisdom;
 }
 
-void Sheet::set_charisma(short t_charisma)
+void Sheet::set_charisma(uint8_t t_charisma)
 {
     if(t_charisma > 0) m_charisma = t_charisma;
 }
 
 // Correct possible negative values to acept negative values that could come
 // from magic debufs.
-void Sheet::set_maxLife(short t_maxLife)
+void Sheet::set_maxLife(unsigned short t_maxLife)
 {
     if (t_maxLife <= 0) m_maxLife = 1u;
     else m_maxLife = t_maxLife;
@@ -96,7 +93,7 @@ void Sheet::set_maxLife(short t_maxLife)
 
 // Correct possible negative values to acept negative values that will come
 // from damages.
-void Sheet::set_currentLife(short t_currentLife)
+void Sheet::set_currentLife(unsigned short t_currentLife)
 {
     if (t_currentLife < 0) m_currentLife = 0;
     else
@@ -113,60 +110,66 @@ void Sheet::set_naturalWeapon(const Weapon& t_naturalWeapon)
     m_naturalWeapon = std::make_unique<Weapon>( t_naturalWeapon );
 }
 
-short Sheet::strength() const
+int Sheet::strength() const
 {
     return m_strength;
 }
 
-short Sheet::dexterity() const
+int Sheet::dexterity() const
 {
     return m_dexterity;
 }
 
-short Sheet::constitution() const
+int Sheet::constitution() const
 {
     return m_constitution;
 }
 
-short Sheet::inteligence() const
+int Sheet::inteligence() const
 {
     return m_inteligence;
 }
 
-short Sheet::wisdom() const
+int Sheet::wisdom() const
 {
     return m_wisdom;
 }
 
-short Sheet::charisma() const
+int Sheet::charisma() const
 {
     return m_charisma;
 }
 
-short Sheet::modifier(uint8_t t_atribute) const
+int Sheet::modifier(int t_atribute)
 {
     if (t_atribute % 2 != 0) t_atribute -= 1;
 
     return (t_atribute - 10) / 2;
 }
 
-const std::string Sheet::showStats() const
+std::string Sheet::showStats() const
 {
     std::stringstream stream;
 
+    // Avoiding code repetition with lambda function.
+    std::function<std::string (std::string, int)> showStat = 
+        [] (const std::string& statName, int value)
+    {
+        std::stringstream stream;
+
+        stream << statName << ": " << value << std::showpos << " ("
+        << modifier(value) << ")\n";
+
+        return stream.str();
+    };
+
     stream
-    << "Strength: " << std::showpos << strength() << " (" << 
-        modifier(strength() ) << ")\n"
-    << "Dexterity: " << std::showpos << dexterity() << " (" << 
-        modifier(dexterity() ) << ")\n"
-    << "Constitution: " << std::showpos << constitution() << " (" << 
-        modifier(constitution() ) << ")\n"
-    << "Inteligence: " << std::showpos << inteligence() << " (" << 
-        modifier(inteligence() ) << ")\n"
-    << "Wisdom: " << std::showpos << wisdom() << " (" << 
-        modifier(wisdom() ) << ")\n"
-    << "Charisma: " << std::showpos << charisma() << " (" << 
-        modifier(charisma() ) << ')';
+        << showStat( "Strength", strength() )
+        << showStat( "Dexterity", dexterity() )
+        << showStat( "Constitution", constitution() )
+        << showStat( "Inteligence", inteligence() )
+        << showStat( "Wisdom", wisdom() )
+        << showStat( "Charisma", charisma() );
 
     return stream.str();
 }
@@ -174,5 +177,6 @@ const std::string Sheet::showStats() const
 std::ostream &operator<<(std::ostream &t_stream, const Sheet &t_sheet)
 {
     t_stream <<t_sheet.show();
+
     return t_stream;
 }
