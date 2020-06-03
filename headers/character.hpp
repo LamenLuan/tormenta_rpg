76ad182f0435af09bd90693f6ea59138336a94c1
Luan Leme <luan.leme.s35@gmail.com>
2020-06-03 08:39:46 -0300

    Removing dexterity( ) method

--- headers/character.hpp
+++ headers/character.hpp
@@ -39,7 +39,6 @@ public:
     void set_equippedShield(const Shield& t_shield);
     void set_coins(unsigned int t_coins);
     
-    int dexterity() const;
     int armorClass() const;
     int initiativeBonus() const;
     void naturalWeapon();
