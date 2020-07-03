01eac2c3b28bc7d5eac79355d63475b0cd898b14
Luan Leme <luan.leme.s35@gmail.com>
2020-07-03 09:31:59 -0300

    Creating store constructor and destructor

--- source/store.cpp
+++ source/store.cpp
@@ -4,15 +4,43 @@ Store::Store(float t_priceMultiplier, StoreType t_storeType)
     :
     m_priceMultiplier(t_priceMultiplier), m_storeType(t_storeType)
 {
+    Item* (* generator) (size_t) = nullptr;
+    size_t quantity = 0;
+
     switch (m_storeType)
     {
-    case StoreType::ARMORER :
-        /* code */
-        break;
-    
-    default:
-        break;
+        case StoreType::WEAPONSMITH :
+        {
+            generator = ItemGenerator::generateWeapon;
+            quantity = ItemGenerator::weaponQuantity;
+        } break;
+
+        case StoreType::ARMORER :
+        {
+            generator = ItemGenerator::generateDefenceItem;
+            quantity =
+                ItemGenerator::armorQuantity + ItemGenerator::shieldQuantity;
+        } break;
+
+        case StoreType::GENERAL :
+        {
+            generator = ItemGenerator::generateItem;
+            quantity = 
+                ItemGenerator::weaponQuantity + ItemGenerator::armorQuantity +
+                ItemGenerator::shieldQuantity;
+        } break;
+    }
+
+    for (size_t i = 0; i < quantity; ++i)
+    {
+        m_items.push_back( generator(i) );
     }
 }
 
-Store::~Store() = default;
+Store::~Store()
+{
+    for (auto &i : m_items)
+    {
+        delete i;
+    }
+}
